dadb7a1f6ca73161f883f74bf9d27a371c3c5fd9
Alexis ANDREANI <alexis.andreani@alten.com>
2022-02-05 23:12:54 +0100

    Second commmit with MonteCarlo Simulation

--- Pricing_Options.py
+++ Pricing_Options.py
@@ -52,6 +52,37 @@ def vanilla_put_price(S, K, r, v, T):
     return -S * norm_cdf(-d_j(1, S, K, r, v, T)) + K * exp(-r * T) * norm_cdf(-d_j(2, S, K, r, v, T))
 
 
+def monte_carlo(prices):
+    returns = prices.pct_change()
+
+    last_price = prices[-1]
+
+    # Number of Simulations
+    num_simulations = 20
+    num_days = 250
+
+    simulation_df = pd.DataFrame()
+
+    for x in range(num_simulations):
+        count = 0
+        daily_vol = returns.std()
+        price_series = []
+        price = last_price * (1 + np.random.normal(0, daily_vol))
+        price_series.append(price)
+        for y in range(num_days):
+            if count == 251:
+                break
+            price = price_series[count] * (1 + np.random.normal(0, daily_vol))
+            price_series.append(price)
+            count += 1
+        simulation_df[x] = price_series
+    return simulation_df
+
+
+def cagr(start_value, end_value, num_periods):
+    return (end_value / start_value) ** (1 / (num_periods - 1)) - 1
+
+
 ## Set up of Streamlit Interface
 st.title('Financial Dashboard')
 # ticker_input = st.text_input('Choose your Type of Option:')
@@ -101,7 +132,7 @@ if add_selectbox == 'Pricing Simulator':
             st.write("In course of production")
 
 elif add_selectbox == "Stocks Viz":
-    st.sidebar.multiselect('What option do you want to add',['SMA', 'UP&Lo BAND', 'Other'])
+    st.sidebar.multiselect('What option do you want to add', ['SMA', 'UP&Lo BAND', 'Other'])
     st.header("Stocks Viz")
     ticker = st.text_input("Enter a ticker")
     button = st.button('Enter')
@@ -113,17 +144,22 @@ elif add_selectbox == "Stocks Viz":
         ## Compute for the widget options
         SMA = Adj_Close.rolling(window=10).mean()
         STD = Adj_Close.rolling(window=10).std()
-        UPPER_BAND = SMA +3*STD
-        LOWER_BAND = SMA -3*STD
-        #Merged = pd.merge(Adj_Close, SMA, on="Date")
-        #Merged = Merged.rename({'Adj Close_x': ticker, 'Adj Close_y': ticker + ' SMA 10'}, axis=1)  # new method
-
-        #Merged_1 = pd.merge(Merged,UPPER_BAND,on="Date")
-        #Merged_1 = Merged_1.rename({'Adj Close_x': ticker, 'Adj Close_y': ticker + ' SMA 10','Adj Close':ticker+'UpperBand'}, axis=1)  # new method
-
-        #print(Merged_1.head())
-        #Merged_2 = pd.merge(Merged_1,LOWER_BAND,on="Date")
-        #Merged_2 = Merged_2.rename({'Adj Close':ticker+'LowerBand'}, axis=1)
-        #print(Merged_2.head(20))
-
+        UPPER_BAND = SMA + 3 * STD
+        LOWER_BAND = SMA - 3 * STD
+        LOG_RETURN = Adj_Close.pct_change()
+        print(LOG_RETURN.head())
+        # Merged = pd.merge(Adj_Close, SMA, on="Date")
+        # Merged = Merged.rename({'Adj Close_x': ticker, 'Adj Close_y': ticker + ' SMA 10'}, axis=1)  # new method
+        # Merged_1 = pd.merge(Merged,UPPER_BAND,on="Date")
+        # Merged_1 = Merged_1.rename({'Adj Close_x': ticker, 'Adj Close_y': ticker + ' SMA 10','Adj Close':ticker+'UpperBand'}, axis=1)  # new method
+        # print(Merged_1.head())
+        # Merged_2 = pd.merge(Merged_1,LOWER_BAND,on="Date")
+        # Merged_2 = Merged_2.rename({'Adj Close':ticker+'LowerBand'}, axis=1)
+        # print(Merged_2.head(20))
+        st.header('Stock Price evolution:')
         st.line_chart(Adj_Close, width=200, height=400, use_container_width=True)
+        st.header('Return of the Stock:')
+        st.line_chart(LOG_RETURN, width=200, height=400, use_container_width=True)
+        st.header('Monte Carlo simulation over 2 years:')
+        MC = monte_carlo(Adj_Close)
+        st.line_chart(MC,width=200, height=400, use_container_width=True)
